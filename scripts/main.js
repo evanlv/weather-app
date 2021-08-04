cbab7de99cd12f10f5bd6d0dea681704fba80dfb
Evan LV <evan@nine1000.com>
2021-08-04 21:35:15 +0200

    feat: add hour & temperature

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 const API_KEY = "2bafbeb367d729d6edb9335e000158bf";
+
+const weatherApp = document.querySelector(".weather-app");
+const temperatureApp = document.querySelector(".temperature-app");
+const locationApp = document.querySelector(".location-app");
+const hour = document.querySelectorAll(".hour-name-prevision");
+const weatherByHour = document.querySelectorAll(".hour-prevision-value");
+
 let resultsApi;
 
 if (navigator.geolocation) {
@@ -7,6 +14,7 @@ if (navigator.geolocation) {
       // console.log(position);
       let long = position.coords.longitude;
       let lat = position.coords.latitude;
+
       callApi(long, lat);
     },
     () => {
@@ -16,12 +24,40 @@ if (navigator.geolocation) {
 }
 const callApi = (long, lat) => {
   fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metrics&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${API_KEY}`
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      resultsApi = data;
       console.log(data);
+      weatherApp.innerText = resultsApi.current.weather[0].description;
+      temperatureApp.innerText = `${Math.trunc(
+        resultsApi.current.temperature
+      )}℃`;
+      locationApp.innerText = resultsApi.timezone;
+
+      // Hours, every 3h, with their temperature
+      let actualHour = new Date().getHours();
+
+      for (let i = 0; i < hour.length; i++) {
+        let hourIncr = actualHour + i * 3;
+
+        if (hourIncr > 24) {
+          hour[i].innerText = `${hourIncr - 24} h`;
+        } else if (hourIncr === 24) {
+          hour[i].innerText = "00 h";
+        } else {
+          hour[i].innerText = `${hourIncr} h`;
+        }
+      }
+
+      //Weather, every 3h
+      for (let j = 0; j < weatherByHour.length; j++) {
+        weatherByHour[j].innerText = `${Math.trunc(
+          resultsApi.hourly[j * 3].temp
+        )}℃`;
+      }
     });
 };
